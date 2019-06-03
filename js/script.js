9177fd87eb56e6411ac0116c3ad9bf9c0b2e72b5
ma1ex <matvienko.alexey@gmail.com>
2019-06-03 23:32:10 +0300

    Расширение функционала; вывод уровня достатка.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let moneyInMonth = +prompt("Ваш бюджет на месяц?", '');
+let moneyInMonth = prompt("Ваш бюджет на месяц?", '');
 console.log("money: " + moneyInMonth);
 
 let time = prompt("Введите дату в формате YYYY-MM-DD", '');
@@ -8,23 +8,46 @@ console.log("time:" + time);
 
 let appData = {
     budget: '',                       // Бюджет на месяц
-    timeDate: '',                     // Дата в формате YYYY-MM-DD
+    timeData: '',                     // Дата в формате YYYY-MM-DD
     expenses: {},                     // Объект с обязательными расходами
     optionalExpenses: {},             // Объект с необязательными расходами
     income : [],                      // Массив данных с доп. доходом
     savings: false,                   //
-    getOneDayBudget: function() {     // Бюджет на один день
-        return Math.floor(this.budget / 30);
+    get oneDayBudget() {              // Бюджет на один день
+        // return Math.floor(this.budget / 30).toFixed(2);
+        return ( parseFloat(this.budget) / 30 ).toFixed(2);
     }
 };
 
 appData.budget = moneyInMonth;
-appData.timeDate = time;
+appData.timeData = time;
 console.log(appData.budget + ', ' + appData.timeData);
 
-let costOfMonth = prompt("Введите обязательную статью расходов в этом месяце", '');
-let costHowMuch = prompt("Во сколько обойдется?", '');
-appData.expenses[costOfMonth] = costHowMuch;
+for (let i = 0; i < 2; i++) {
+    while(true) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+            
+        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && 
+                a != '' && b != '' && a.length < 50 ) {
+            console.log("Done!");
+            appData.expenses[a] = b;
+            break;
+        } else {
+            alert("Необходимо ввести обязательную статью расходов!");
+        }
+    }
+}
 console.log(appData.expenses);
 
-alert("Ваш бюджет на один день: " + appData.getOneDayBudget() + ' рублей.');
+alert("Ваш бюджет на один день: " + appData.oneDayBudget + ' рублей.');
+
+if (appData.oneDayBudget < 100) {
+    console.log("У Вас минимальный уровень достатка.");
+} else if (appData.oneDayBudget > 100 && appData.oneDayBudget < 700) {
+    console.log("У Вас сердний уровень достатка.");
+} else if (appData.oneDayBudget > 700) {
+    console.log("У Вас высокий уровень достатка.");
+}
+
+console.log(appData);
